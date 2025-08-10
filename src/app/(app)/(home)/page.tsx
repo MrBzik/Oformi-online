755512e21fe0536da9a71ddb803a9d04c8ac45db
U-int-32 <sysctlbyname@gmail.com>
2025-08-10 14:28:45 +0300

    after init mongodb

---
+++ src/app/(app)/(home)/page.tsx
@@ -0,0 +1,33 @@
+import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
+import {Progress} from "@/components/ui/progress";
+import {Textarea} from "@/components/ui/textarea";
+import {Checkbox} from "@/components/ui/checkbox";
+
+
+export default function Home() {
+  return (
+      <div className="p-4">
+          <div className="flex flex-col gap-y-4">
+              <div>
+                  <Button>
+                      The button
+                  </Button>
+              </div>
+              <div>
+                  <Input placeholder="I am an input"/>
+              </div>
+              <div>
+                  <Progress value={50}/>
+              </div>
+              <div>
+                  <Textarea placeholder="I am a text area"/>
+              </div>
+              <div>
+                  <Checkbox/>
+              </div>
+          </div>
+      </div>
+
+  );
+}
