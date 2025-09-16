6b64e27e7827e36fb8328ff1df480aa4f2dae775
U-int-32 <sysctlbyname@gmail.com>
2025-09-16 12:42:13 +0300

    payload vercel blob

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -20,7 +20,6 @@ import {Orders} from "@/collections/Orders";
 import {isSuperAdmin} from "@/lib/access";
 import {resendAdapter} from "@payloadcms/email-resend";
 import {Favourite} from "@/collections/Favourite";
-import {uploadthingStorage} from "@payloadcms/storage-uploadthing";
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -74,21 +73,12 @@ export default buildConfig({
       },
       userHasAccessToAllTenants: (user) => isSuperAdmin(user),
     }),
-    // vercelBlobStorage({
-    //   enabled: true,
-    //   collections: {
-    //     media: true
-    //   },
-    //   token: process.env.BLOB_READ_WRITE_TOKEN
-    // })
-    uploadthingStorage({
+    vercelBlobStorage({
+      enabled: true,
       collections: {
-        media: true,
+        media: true
       },
-      options: {
-        token: process.env.UPLOADTHING_TOKEN,
-        acl: 'public-read',
-      },
-    }),
+      token: process.env.BLOB_READ_WRITE_TOKEN
+    })
   ],
 })
