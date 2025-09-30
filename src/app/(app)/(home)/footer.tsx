a3bcf473ce5a952418fbfaea3b17346a328647ad
U-int-32 <sysctlbyname@gmail.com>
2025-09-30 11:48:09 +0300

    footer type fix

--- src/app/(app)/(home)/footer.tsx
+++ src/app/(app)/(home)/footer.tsx
@@ -10,7 +10,7 @@ export const Footer = () => {
                         Оформи онлайн
                     </span>
             </Link>
-me            <Link href="https://t.me/MessageOoBot?start=sup" className="cursor-pointer text-input-variant">Написать в поддержку</Link>
+            <Link href="https://t.me/MessageOoBot?start=sup" className="cursor-pointer text-input-variant">Написать в поддержку</Link>
         </footer>
     )
 }
