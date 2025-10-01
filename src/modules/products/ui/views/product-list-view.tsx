1695e983e7b998537794a41e3759e7b3c454d8d5
U-int-32 <sysctlbyname@gmail.com>
2025-10-01 09:43:20 +0300

    fixed product list gap x

--- src/modules/products/ui/views/product-list-view.tsx
+++ src/modules/products/ui/views/product-list-view.tsx
@@ -66,7 +66,7 @@ export const ProductListView = ({
                 }
                 <ProductSort/>
             </div>
-            <div className="grid grid-cols-1 lg:grid-cols-7 xl:grid-cols-9 gap-y-6 gap-x-12 ">
+            <div className="grid grid-cols-1 lg:grid-cols-7 xl:grid-cols-9 gap-y-6 lg:gap-x-12 ">
                 {isDisplayFilters && (
                     <div className="col-span-2">
                         <ProductFilters category={category}/>
