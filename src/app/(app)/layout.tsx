40c09f6e6363c29363003928c882dc9fec4eb9e4
U-int-32 <sysctlbyname@gmail.com>
2025-10-05 16:40:37 +0300

    back to lib

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -5,7 +5,7 @@ import {TRPCReactProvider} from "@/trpc/client";
 import {Toaster} from "@/components/ui/sonner";
 import {NuqsAdapter} from "nuqs/adapters/next/app";
 import BottomNav from "@/modules/shared/ui/components/bottom-nav";
-import Script from "next/script";
+import {YandexMetricaProvider} from "next-yandex-metrica";
 
 const dmSans = DM_Sans({
     subsets: ["latin"],
@@ -37,34 +37,15 @@ export default function RootLayout({
         <body
             className={`${dmSans.className} antialiased`}
         >
-        <Script
-            id="yandex-metrica"
-            strategy="afterInteractive" // ensures it runs after hydration
-            dangerouslySetInnerHTML={{
-                __html: `
-              (function(m,e,t,r,i,k,a){
-                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                m[i].l=1*new Date();
-                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104387626', 'ym');
-
-              ym(104387626, 'init', {ssr:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
-            `,
-            }}
-        />
-        <noscript>
-            <div>
-                <img
-                    src="https://mc.yandex.ru/watch/104387626"
-                    style={{ position: 'absolute', left: '-9999px' }}
-                    alt=""
-                />
-            </div>
-        </noscript>
         <NuqsAdapter>
             <TRPCReactProvider>
-                {children}
+                <YandexMetricaProvider
+                    tagID={104387626}
+                    initParameters={{ clickmap: true, trackLinks: true, accurateTrackBounce: true }}
+                    router="app"
+                >
+                    {children}
+                </YandexMetricaProvider>
                 <Toaster/>
                 <BottomNav/>
             </TRPCReactProvider>
